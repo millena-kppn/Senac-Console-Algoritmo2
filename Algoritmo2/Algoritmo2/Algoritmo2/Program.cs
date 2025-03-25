2395ff7f58fd1b078013b40238fbb9eca90ab5ec
Millena Kappaun <kappaunmillena@gmail.com>
2025-03-25 20:36:34 -0300

    Criação das variaveis

--- Algoritmo2/Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Algoritmo2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int idade = 10;
+string nomeCliente = "Millena";
+decimal preco = 20M;
+char letra = 'a';
+var nomeCompleto = "Millena Kappaun";
+var altura = 180;
