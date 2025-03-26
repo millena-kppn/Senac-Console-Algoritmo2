64f68194a511be42b824e79e5460f6c30d529e2a
Millena Kappaun <kappaunmillena@gmail.com>
2025-03-25 21:51:05 -0300

    Leitura de variaveis

--- Algoritmo2/Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Algoritmo2/Program.cs
@@ -4,3 +4,15 @@ decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Millena Kappaun";
 var altura = 180;
+Console.WriteLine("Digite seu nome:");//imprimi uma mensagem
+nomeCompleto = Console.ReadLine();
+Console.WriteLine("Bem vindo " + nomeCompleto + "!");
+Console.WriteLine();
+Console.Write("Digite a sua idade...");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);// int.Parse converter um numero inteiro (rastrear)
+Console.WriteLine("Idade Informada " + idade);
+if (idade < 18)
+    Console.WriteLine("Você não pode entrar");
+Console.ReadKey();//vai esperar uma ação para encerrar o programa
+//Console.ReadLine();//consigo capturar os dados
